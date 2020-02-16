fbf8cb583abd48aa84846384f5eca4165a83931c
“Maryfrances-U” <mumeora@u.rochester.edu>
2020-02-16 19:02:00 +0000

    Module_Two

--- djangoProj/quiz/models.py
+++ djangoProj/quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+    quiz_title = models.CharField(max_length=50)
+    quiz_description = models.CharField(max_length=100)
+    quiz_difficulty = models.IntegerField()
+
+class Question(models.Model):
+    question_title = models.CharField(max_length=50)
+    question_text = models.CharField(max_length=100)
+    is_multi_answer = models.BooleanField()
+    quiz_foreign_key = models.IntegerField()
+
+class Answer(models.Model):
+    answer_title = models.CharField(max_length=50)
+    answer_description = models.CharField(max_length=100)
+    is_correct_answer = models.BooleanField()
+    number_of_points = models.IntegerField()
+    question_foreign_key = models.IntegerField()
+
